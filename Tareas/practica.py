5eae6b39d2e32eeb21d971b0ed45261b1d8e09b8
Herasi Silva <herasidesweb@gmail.com>
2025-07-08 23:36:56 -0400

    aprendiendo y repasando

--- Tareas/practica.py
+++ Tareas/practica.py
@@ -1,9 +1,20 @@
-try:
-  document = open("Tareas/history.txt", "r")
-  print(document.read())
-except Exception as e:
-  print(e)
-finally:
-  document.close()
+ # try:
+ #   document = open("Tareas/history.txt", "r")
+  #  print(document.read())
+  #  document.close()
+ # except Exception as e:
+  #  print(e)
+    
 
-  
+#crear un archivo
+
+try:
+    document = open("Tareas/python.txt", "w")
+    document.write("Hola, este estoy aprendiendo Python.")
+    document.close()
+
+    document = open("Tareas/python.txt", "r")
+    print(document.read())    
+    document.close()
+except Exception as e:
+    print(e)
